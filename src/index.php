20287a756e89b5ac820a940d8ceeed5b22465609
sporeventexplosion <sporeventexplosion@gmail.com>
2015-05-22 16:41:25 +0300

    Add basic JavaScript

--- src/index.php
+++ src/index.php
@@ -5,9 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pegasus Cloud</title>
-    <?php
-    	echo "3k.".$_GET["gooda"]."f";
-    ?>
+    <script src="/assets/js/main.js"></script>
   </head>
   <body>
     <h1>FOUEC</h1>
